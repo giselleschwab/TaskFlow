018312cadd75b08d111230bca35f41c25bed6c4b
Giselle Schwab Silva <giselleschwab@gmail.com>
2025-03-25 10:51:19 -0300

    feat: improve draggable task cards and dropdown styling

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useMemo } from "react";
 import axios from "axios";
 import { DndContext, useDroppable, useDraggable, DragEndEvent } from '@dnd-kit/core';
+import { FaRegClock, FaEllipsisH } from 'react-icons/fa';
 
 interface Task {
   id: number;
@@ -26,9 +27,14 @@ const TaskList: React.FC = () => {
       });
   }, []);
 
-  const filterTasksByStatus = (status: string) => {
-    return tasks.filter(task => task.status === status);
-  };
+  const groupedTasks = useMemo(() => {
+    return {
+      pendente: tasks.filter(task => task.status === 'pendente'),
+      "em andamento": tasks.filter(task => task.status === 'em andamento'),
+      concluída: tasks.filter(task => task.status === 'concluída'),
+    };
+  }, [tasks]);
+
 
   const onDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
@@ -46,7 +52,6 @@ const TaskList: React.FC = () => {
 
     setTasks(updatedTasks);
 
-    // Enviar para o backend
     axios.post("http://localhost/tasks/update-status", {
       id: taskId,
       status: over.id
@@ -61,24 +66,32 @@ const TaskList: React.FC = () => {
 
 
   return (
-    <div className="flex justify-center items-centerbg-dark-gray pt-12">
-      <div className="w-full max-w-7xl px-4">
-        <div className="flex justify-between items-center border-b border-gray-400 mb-4">
-          <div>
-            <h1 className="text-white text-xl">Minhas Tarefas</h1>
-            <h2 className="text-white mb-4">Aqui vou colocar uma legenda</h2>
+    <>
+      <div className="flex justify-center items-centerbg-dark-gray pt-12">
+        <div className="w-full max-w-7xl px-4">
+          <div className="flex justify-between items-center border-b border-gray-400 mb-4">
+            <div>
+              <h1 className="text-white text-xl">Minhas Tarefas</h1>
+              <h2 className="text-white mb-4">Aqui vou colocar uma legenda</h2>
+            </div>
           </div>
         </div>
 
-        <DndContext onDragEnd={onDragEnd}>
-          <div className="flex gap-8 pt-8">
-            <Column id="pendente" title="Pendentes" tasks={filterTasksByStatus('pendente')} />
-            <Column id="em andamento" title="Em Progresso" tasks={filterTasksByStatus('em andamento')} />
-            <Column id="concluída" title="Concluídas" tasks={filterTasksByStatus('concluída')} />
-          </div>
-        </DndContext>
       </div>
-    </div>
+
+      <DndContext onDragEnd={onDragEnd}>
+        <div className="bg-[#171616] mx-14 rounded-md ">
+          <div className="flex justify-items-center gap-4 px-4 pt-4">
+            <Column id="pendente" title="Pendentes" tasks={groupedTasks.pendente} />
+            <Column id="em andamento" title="Em Progresso" tasks={groupedTasks["em andamento"]} />
+            <Column id="concluída" title="Concluídas" tasks={groupedTasks.concluída} />
+
+          </div>
+        </div>
+
+      </DndContext>
+    </>
+
   );
 };
 
@@ -92,8 +105,8 @@ const Column: React.FC<ColumnProps> = ({ id, title, tasks }) => {
   const { setNodeRef } = useDroppable({ id }); // Garante que a coluna é "droppable"
 
   return (
-    <div className="w-1/3" ref={setNodeRef}>
-      <h3 className="text-white text-lg">{title}</h3>
+    <div className="w-1/3 bg-[#2E2E2E] p-4 mb-4 rounded" ref={setNodeRef}>
+      <h3 className="text-white text-lg border-b border-[#69B9B6]">{title}</h3>
       <ul className="text-white">
         {tasks.map((task) => (
           <TaskCard key={task.id} task={task} /> // Passa as tarefas para cada TaskCard
@@ -109,10 +122,38 @@ interface TaskCardProps {
 
 const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
   const { attributes, listeners, setNodeRef, isDragging, transform } = useDraggable({
-    id: String(task.id), // Usar o id da tarefa como identificador único para o drag
+    id: String(task.id),
   });
 
-  // Estilo aplicado quando a tarefa está sendo arrastada
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+  
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setIsOpen(false);
+      }
+    };
+  
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("keydown", handleKeyDown);
+    }
+  
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen]);
+
   const draggingStyle = {
     cursor: 'grabbing',
     transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : 'none',
@@ -122,15 +163,46 @@ const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
 
   return (
     <li
-      ref={setNodeRef} // Ref para o item arrastável
+      ref={setNodeRef}
       {...attributes}
       {...listeners}
-      className="mb-2 bg-gray-800 p-4 rounded-md cursor-pointer"
+      className="mb-2 bg-[#020202]/80 p-4 rounded-md cursor-pointer mt-3"
       style={isDragging ? draggingStyle : {}}
     >
-      <h4>{task.task_name}</h4>
-      <p>{task.description}</p>
-      <span className="text-sm text-gray-400">{task.status}</span>
+
+      <div className="relative">
+        <div className="flex justify-between items-center">
+          <h4 className="text-[#C1C1C1] text-lg">{task.task_name}</h4>
+          <FaEllipsisH
+            className="text-[#C1C1C1] cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+            onPointerDown={(e) => e.stopPropagation()}
+          />
+        </div>
+
+        {isOpen && (
+          <div ref={dropdownRef} className="absolute right-0 w-40 bg-[#2E2E2E] rounded-md shadow-lg z-50">
+            <ul className="py-2">
+              <li className="px-4 py-2 hover:bg-[#383838] cursor-pointer" onClick={() => console.log("Editar")}>
+                Editar
+              </li>
+              <li className="px-4 py-2 hover:bg-[#383838] cursor-pointer" onClick={() => console.log("Excluir")}>
+                Excluir
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+
+      <p className="text-[#C1C1C1] pt-4">{task.description}</p>
+
+      <div className="pt-4 mt-6 mb-2">
+        <span className="text-sm text-gray-400 font-semibold bg-[#2C2C2C] p-3 pr-4 rounded-full">
+          <FaRegClock className="inline-block font-bo text-[#C1C1C1] mr-2 mb-0.5" />
+          19/04/2025</span>
+      </div>
+
     </li>
   );
 };
